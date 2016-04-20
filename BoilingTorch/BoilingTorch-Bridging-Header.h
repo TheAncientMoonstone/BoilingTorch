536af2342208513f483ef94bca4229badc24c7bc
TheAncientMoonstone <TheAncientMoonstone@users.noreply.github.com>
2016-04-21 00:34:11 +0800

    Delete BoilingTorch-Bridging-Header.h

--- BoilingTorch/BoilingTorch-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <Firebase/Firebase.h>
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <FBSDKLoginKit/FBSDKLoginKit.h>
-#import <Tapglue/Tapglue.h>
